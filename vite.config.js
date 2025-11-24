35dbe1d74b6d23af0b0d00b39f90de99da97e4e6
iamrazzakk <rakibt23p@gmail.com>
2025-11-24 18:34:21 +0600

    refactor vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     // host: "10.10.7.46",
-    host: "193.46.198.251",
+    // host: "193.46.198.251",
+    host: "https://admin.agritecint.com",
     port: 3001,
   },
 });
