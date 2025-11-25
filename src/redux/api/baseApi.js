85db0f71286830aba856cf8281e134b2ab301a82
iamrazzakk <rakibt23p@gmail.com>
2025-11-25 18:43:31 +0600

    asdfasdf

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,21 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        // baseUrl: "http://10.10.7.46:5001/api/v1",
-        baseUrl: "http://193.46.198.251:5001/api/v1",
-        prepareHeaders: (headers) => {
-            const token = localStorage.getItem("accessToken");
-            if (token) {
-                headers.set("Authorization", `Bearer ${token}`);
-            }
-            return headers;
-        },
-    }), 
-    tagTypes: ["Auth"],
-    endpoints: () => ({})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    // baseUrl: "http://10.10.7.46:5001/api/v1",
+    baseUrl: "https://api.agritecint.com/api/v1",
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Auth"],
+  endpoints: () => ({}),
 });
 
-export const imageUrl = "http://193.46.198.251:5001/";
+export const imageUrl = "https://api.agritecint.com";
 // export const imageUrl = "http://10.10.7.46:5001";
